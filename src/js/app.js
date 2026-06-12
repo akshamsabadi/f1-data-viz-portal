8d8425947ee9cd83238a42ad03b74bec96993a8b
André Shamsabadi <ashamsabadi@isomorphiclabs.com>
2026-06-12 14:25:13 +0000

    Change layout: display all charts vertically at full width

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import { renderBeeswarm } from './beeswarm.js';
 import { renderCornerSpeed } from './corner_speed.js';
 import { renderBump } from './bump.js';
 
-const APP_VERSION = 'v1.11.0';
+const APP_VERSION = 'v1.12.0';
 let currentData = null;
 
 async function init() {
