74ac264d578420d235ff1c162179d5871269ad7e
André Shamsabadi <ashamsabadi@isomorphiclabs.com>
2026-06-12 13:36:11 +0000

    Remove mock future races to strictly only show valid 2026 telemetry data

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import { renderBeeswarm } from './beeswarm.js';
 import { renderInterval } from './interval.js';
 import { renderBump } from './bump.js';
 
-const APP_VERSION = 'v1.7.0';
+const APP_VERSION = 'v1.8.0';
 let currentData = null;
 
 async function init() {
