51b35ec7610e718cbe9de11f736975a955fb0dd8
André Shamsabadi <ashamsabadi@isomorphiclabs.com>
2026-07-11 11:20:32 +0000

    UI: Align animated loader concentric rings with mathematical precision

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import { renderBeeswarm } from './beeswarm.js';
 import { renderCornerSpeed } from './corner_speed.js';
 import { renderBump } from './bump.js';
 
-const APP_VERSION = 'v1.16.18';
+const APP_VERSION = 'v1.16.19';
 
 const OFFICIAL_COLORS = {
     "Ferrari": "#e50004",
